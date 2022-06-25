ee55c904b03b313a01265da8b0b223f32b4dd7e7
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2022-06-25 01:59:30 -0600

    Empezando a leer el JSON en Angular

--- Angular/src/app/admin/admin.component.ts
+++ Angular/src/app/admin/admin.component.ts
@@ -6,13 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  contenido=""
   constructor() { }
 
   ngOnInit(): void {
   }
   consulta(){
     alert("Aun soy digno");
+  }
+
+  abrir(event:any) {
+    const file = event.target.files[0];
+   
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      //console.log(fileReader.result);
+      if(fileReader.result!=null){
+        this.contenido=fileReader.result.toString()
+        this.transformarJSON(this.contenido);
+      }
+    }
+    fileReader.readAsText(file)   
+  }
+
+  transformarJSON(inicial:string){
+    let obj = JSON.parse(inicial);
+    /*for (let i = 0; i < obj.carpetas.length; i++) {
+      console.log(obj.carpetas[i]);
+      
+    }*/
+    for (let i = 0; i < obj.archivos.length; i++) {
+      console.log(obj.archivos[i]);
+      
+    }
   }
 }
 
